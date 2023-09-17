c20de8392a18ef32c879544252b489ee3c1c42e1
MayooriMohan <144256571+MayooriMohan@users.noreply.github.com>
2023-09-17 23:30:15 +0530

    Add files via upload

---
+++ Mayoori Mohan project 1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  6 00:00:24 2023
+
+@author: ASUS
+"""
+
+
+radius = float(input("Input the radius of the circle: "))
+pi = 3.14159
+area = pi * (radius ** 2)
+print(f"The area of the circle with radius {radius} is: {area:.16f}")
+
+filename = input("Enter a file name: ")
+print(f"The extension of the file is:.py")
+    
